14e63d4df755c0e0eae0f5223100a2bf316082a7
Ernest Wambua <ernestwambua2@gmail.com>
2022-08-23 09:43:56 +0300

    binary_tree_print

--- binary_trees.h
+++ binary_trees.h
@@ -21,5 +21,6 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif
